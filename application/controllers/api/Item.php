179f6cf375e3538dcc7eb8ee8e38ad3b6f379876
Yohanes Boyoh <boyohh@hotmail.com>
2018-07-29 18:28:07 +0700

    API add new item

--- application/controllers/api/Item.php
+++ application/controllers/api/Item.php
@@ -30,6 +30,24 @@ API updateItem
       "description":"Indomie telor bakso test"
   	}
 }
+
+API addNewItem
+{
+	"username":"admin",
+	"token":"805e5a8c7eb7517f863718274716a734",
+  	"data": {
+      "data_update": {
+        "name":"Bakso",
+        "description":"Bakso Ikan Sushi",
+        "barcode":"baksocode",
+        "price_supplier":1300,
+        "price_customer":1400,
+        "qty_stock":10,
+        "user_created":1
+      }, 
+      "id_supplier":2
+  	}
+}
 */
 
 
@@ -40,6 +58,7 @@ class Item extends CI_Controller {
         error_reporting(0);
         $this->load->helper(array('form', 'url','security','date'));
         $this->load->model('UserModel'); 
+        $this->load->model('StockModel'); 
         $this->load->model('ItemModel'); 
         $this->load->model('ErrorStatusModel');    
     }
@@ -184,6 +203,65 @@ class Item extends CI_Controller {
 			echo $jsonEncodeResponse;
 		}
     }
+
+	public function addNewItem() {
+		try {
+			// RECEIVE REQUEST
+			$json = file_get_contents('php://input');
+			$dataJsonRequest = json_decode($json);
+			// GET JSON REQUEST
+			$p_token = $dataJsonRequest->token;
+			$p_username = $dataJsonRequest->username;
+			$p_new_data = $dataJsonRequest->data->data_update;
+			$p_id_supplier = $dataJsonRequest->data->id_supplier;
+	        // CEK USER TOKEN VALID
+			$this->isUserValidToken($p_token, $p_username);
+			// GET USER
+	        $result_user = $this->UserModel->getUserDetail($p_username);
+			if(empty($result_user)) {
+	        	$error_code = -2;
+	        	throw new Exception($this->setErrorMessage(-2), -2);
+	        }
+	        $this->db->trans_start();
+	        // MASUKIN NEW ITEM
+	        $dataNewItem = $p_new_data;
+	        $id_item = $this->ItemModel->createItem($dataNewItem);
+	        if(empty($id_item)) {
+				$isSuccess[] = false;
+	        }
+	        $price_total_supplier = $p_new_data->qty_stock * $p_new_data->price_supplier;
+	        $data_stock[] = array(	'id_item' => $id_item,
+					        		'type_trans' => "add_stock",
+        							'id_supplier' => $p_id_supplier,
+        							'qty_trans' => $p_new_data->qty_stock,
+        							'price_total_supplier' => $price_total_supplier,
+        							'last_qty_stock' => 0,
+        							'current_qty_stock' => $p_new_data->qty_stock,
+        							'status' => 0,
+        							'user_created' => $p_new_data->user_created
+        							);
+	        // MASUKIN STOK BATCH
+	        $isSuccess[] = $this->StockModel->addStockBatch($data_stock);
+	        if (in_array(false, $isSuccess)) {
+	        	throw new Exception($this->setErrorMessage(-4), -4);
+	        }
+	        $this->db->trans_complete();
+			$jsonEncodeResponse = json_encode(array('rescode' => 0,
+													'resmessage' => $this->setErrorMessage(0)), JSON_UNESCAPED_SLASHES
+												);
+		} catch (Exception $e) {
+			$error_code = $e->getCode();
+			$error_text = $e->getMessage();
+			$jsonEncodeResponse = json_encode(array( 	'resCode' => $error_code,
+							                            'resMessage' => $error_text
+							                        ), JSON_UNESCAPED_SLASHES
+												);	
+		} finally {
+			echo $jsonEncodeResponse;
+		}
+
+    }
+
 
     private function isUserValidToken($p_token, $p_username) {
 		if(empty($this->UserModel->isToken($p_token, $p_username))) {
